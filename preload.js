3b78efa84022a27516509baaef37f5c60ddd5c03
Darya Sajjad <gorahodarya@gmail.com>
2026-07-16 13:00:42 +0500

    connected to spotify - controller functionality works

--- preload.js
+++ preload.js
@@ -1,6 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-// Securely expose native APIs to the renderer process
 contextBridge.exposeInMainWorld('electronAPI', {
-  // You will add IPC functions here later (e.g., to read local music files)
+  getAccessToken: () => ipcRenderer.invoke('spotify-get-token'),
+  logout: () => ipcRenderer.invoke('spotify-logout'),
 });
